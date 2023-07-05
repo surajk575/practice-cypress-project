e340c9a29f9c2cc0f5ced3b7f56f9217942fed74
Suraj <surajkarbhari12345@gmail.com>
2023-07-05 20:45:28 +0530

    new dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+  projectId: "w38vwy",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
